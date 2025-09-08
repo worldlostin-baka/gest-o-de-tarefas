3c37202d763db310d5675d6da254adfac2c42b30
worldlostin-baka <penggist19@gmail.com>
2025-09-08 11:32:32 +0100

    summary

--- sistema/gestao-reservas-frontend/vite.config.js
+++ sistema/gestao-reservas-frontend/vite.config.js
@@ -1,23 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  plugins: [react()],
   server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-        secure: false,
-      }
-    }
+    port: 3000,
+    open: true
   }
 })
